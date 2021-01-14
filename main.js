fbde8620004797d07e50047a9a2df505e29b7684
natchez <natchezzz@hotmail.com>
2021-01-14 13:56:01 +0100

    fin

---
+++ main.js
@@ -0,0 +1,85 @@
+// let reverse = (a) => parseInt(String(54321)
+//     .split("")
+//     .reverse()
+//     .join(""), );
+// console.log(reverse(54321));
+// console.log('____________________________')
+
+
+
+// let nbr = parseInt(prompt("un nombre ?"))
+// let c = nbr/2
+// let div2 = (b) => {
+//     if (b % 2 == 0) {
+//         return console.log(`Le num ${b} est divisible par 2, ${b}:2 = ${c}`)
+//     } else {
+//         return console.log("Le num est impaire")
+//     }
+// }
+// console.log(div2(nbr))
+
+// let a = prompt(`MDP ?`)
+// let login =(mdp) =>{
+//     if (mdp == 'mdp') {
+//         alert("vous êtes connecté")
+//     } else {
+//     a = prompt(`MDP ?`)
+//     }
+// }
+
+// let mdp = prompt("Mot de passe ?")
+
+// let logIn = () => {
+//     while (mdp !== "mdp") {
+//         mdp = prompt("Mot de passe ?")
+//     }
+//     alert("Vous êtes connecté")
+//     return mdp
+// }
+
+// console.log(logIn());
+// c16 = ['Ayhan', 'Benedicte', 'Louise', 'Jamila', 'Sebastien', 'Adame', 'Dawid', 'Malo', 'Alex', 'Maxence', 'Mohammed', 'Saliou', 'Zulma', 'Maximilien', 'Natchez', 'Zakaria', 'Ayoub', 'Achraf', 'Abdellah']
+// let c16bis = []
+// let PUSH = (a, b) => {
+//     let entrer = prompt(`il est ${a}, ${c16[b]} peut rentrer en classe ? oui ou non ?`)
+//     switch (entrer) {
+//         case 'oui':
+//             c16bis.push(c16[b]);
+//             console.log(c16bis);
+//             break;
+//         default:
+//             alert(`ok ${c16[b]} reste dehors`)
+//             console.log(c16bis);
+//             break;
+//     }
+// }
+// let PUSH2 = (a, b,c) => {
+//     let entrer = prompt(`il est ${a}, ${c16[i]} et ${c16[i]} peut rentrer en classe ? oui ou non ?`)
+//     switch (entrer) {
+//         case 'oui':
+//             c16bis.push(c16[b]);
+//             c16bis.push(c16[c]);
+
+//             console.log(c16bis);
+//             break;
+//         default:
+//             alert(`ok ${c16[b]} reste dehors`)
+//             console.log(c16bis);
+//             break;
+//     }
+// }
+
+// PUSH('8h43', 0)
+// PUSH('8h44', 1)
+// PUSH2('8h51', 4,5)
+// PUSH2('8h45', 2,3)
+// puSH2('8h55', 6,7)
+// PUSH('8h56', 8)
+// PUSH2('8h59', 9, 10)
+// PUSH('9h00', 11)
+// PUSH2('9h03', 12,13)
+// PUSH('9h04', 14)
+// PUSH2('9h05', 15,16)
+// PUSH('9h10', 17)
+// alert(`Il est 9h20 les coachs commencent a donner cours`)
+// PUSH('9h30', 18)
